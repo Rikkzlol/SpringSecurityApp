6ca2f90388df5fb2cb25d3cfa26b416312b41764
Rikkzlol <89975272+Rikkzlol@users.noreply.github.com>
2023-08-05 17:43:08 +0300

    -method to user info

--- src/main/java/ru/malyushov/SpringSecurityApp/controllers/HelloController.java
+++ src/main/java/ru/malyushov/SpringSecurityApp/controllers/HelloController.java
@@ -1,8 +1,12 @@
 package ru.malyushov.SpringSecurityApp.controllers;
 
 
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.malyushov.SpringSecurityApp.security.PersonDetails;
 
 @Controller
 public class HelloController {
@@ -10,5 +14,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
         return "/hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails)authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        return "hello";
     }
 }
