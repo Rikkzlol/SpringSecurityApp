b533d3fb38ba70ee869891e97de5a9c0bbee7e06
Rikkzlol <89975272+Rikkzlol@users.noreply.github.com>
2023-08-07 14:41:16 +0300

    -add AuthCOntroller and config custom int SC.

--- src/main/java/ru/malyushov/SpringSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/malyushov/SpringSecurityApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.malyushov.SpringSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -28,7 +29,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //конфигурируем сам спринг секьюрити
+        //конфигурируем авторизацию
+        http.csrf().disable()//отключаем защиту от межайтовой подделки
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
     //настраивает аутентификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
